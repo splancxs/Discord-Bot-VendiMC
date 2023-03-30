6116d5b8d9b371b45d86b6457c0d020a3d21002a
splancxs <romanseby43@gmail.com>
2023-03-30 20:40:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "990012348209172562", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "MTA5MDcyOTgzMzM5NjQ0NTI0NQ.GTKP3I.GpgTxvLS47aVRllnQNP7jbNnPwOw9t0sAucT8s", //- Bot's Token
+	token: process.env.token || "MTA5MDcyOTgzMzM5NjQ0NTI0NQ.GJSICi.qwgx2Yu3tuuUCgwl2Xj-Mf6icehNTX1zXvlITU", //- Bot's Token
 	clientId: process.env.clientId || "1090729833396445245", //- ID of the bot
 	clientSecret: process.env.clientSecret || "p2FMLtTqqQ9gzRwOFPdxxGX9ZxmYrhTI", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
